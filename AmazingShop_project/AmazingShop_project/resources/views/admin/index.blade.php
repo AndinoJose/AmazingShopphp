499ac7bcc149383d5f20804f053898fc7c1e5660
andinojose <jandino820@yahoo.com>
2022-08-15 23:15:13 -0600

    JCcommit
    Commit

---
+++ AmazingShop_project/AmazingShop_project/resources/views/admin/index.blade.php
@@ -0,0 +1,24 @@
+
+    @extends('adminlte::page')
+
+@section('title', 'AmazingShop')
+
+@section('content_header')
+    <h2>AmazingShop</h2>
+@stop
+
+@section('content')
+    <p>Aqui va el contenido de la pagina</p>
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
+
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
+
+
+
